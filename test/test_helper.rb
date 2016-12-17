90389f361b3394fc64cc28ea7ab68f9de8888395
Geoff Hubbard <ghub@instihost.com>
2016-12-17 18:24:22 +0000

    bin/generator: Big rewrite
    Can now generate tests without updating the test version number.
    
    New command line options:
    ```
    Usage: bin/generate [options] exercise-generator
    	-f, --freeze                     Don't update test version
    	-h, --help                       Prints this help
    	-v, --verbose                    Display progress messages
    ```
    
    `sha1` in the "Common test data version: <%= sha1 %>" comment is now the
    commit ID of the relevant canonical-data.json file
    
    Split code up into classes.
    100% test coverage.
    
    Added dependency: `require_all` gem
    
    Just requiring files in (potentially random) directory order was not
    sufficient to satisfy all class dependencies.
    
    The `require_all` gem ensures dependencies are met.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 
 require 'minitest/autorun'
-require 'minitest/pride'
 
 unless ENV['CI']
   require 'simplecov'
-
   SimpleCov.start do
     add_filter '/test/'
-    add_group 'Utilities' do |file|
-      !(file.filename =~ /_cases\.rb$/)
+    add_group 'Generator' do |file|
+      file.filename =~ /generator/
     end
     add_group 'Cases', '_cases.rb'
+    add_group 'Other' do |file|
+      !(file.filename =~ /_cases\.rb$/) && file.filename !~ /generator/
+    end
   end
 end
 
 # So we can be sure we have coverage on the whole lib directory:
-Dir.glob('lib/*.rb').each { |file| require file.gsub(%r{(^lib\/|\.rb$)}, '') }
+Dir.glob('lib/**/*.rb').each { |file| require file.gsub(%r{(^lib\/|\.rb$)}, '') }
