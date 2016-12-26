4eafc13dea65a681c1c91d3eea4695a4164a8fa1
kotp <keeperotphones@gmail.com>
2016-12-26 04:18:59 -0500

    Simplify language of variable names
    If there is no `output_digits` then we don't need to specify, the input
    digits are assumed.
    
    Simplified use of the call, it will take named or positional argument.

--- lib/all_your_base_cases.rb
+++ lib/all_your_base_cases.rb
@@ -6,7 +6,7 @@ class AllYourBaseCase < OpenStruct
   def workload
     (
       [
-        "digits = #{input_digits}",
+        "digits = #{digits}",
         indent(4, "input_base = #{input_base}"),
         indent(4, "output_base = #{output_base}"),
       ] + assertion
@@ -57,7 +57,7 @@ end
 
 class AllYourBaseCase::PreProcessor
   class << self
-    def call(row:)
+    def call(r, row: r)
       @row = row
 
       row.merge('expected' => expected_value)
@@ -70,9 +70,9 @@ class AllYourBaseCase::PreProcessor
     def expected_value
       return row['expected'] if row['expected']
 
-      if invalid_input_digits? || invalid_bases?
+      if invalid_digits? || invalid_bases?
         nil
-      elsif row['input_digits'].empty?
+      elsif row['digits'].empty?
         []
       elsif input_of_zero?
         [0]
@@ -81,8 +81,8 @@ class AllYourBaseCase::PreProcessor
       end
     end
 
-    def invalid_input_digits?
-      row['input_digits'].any? { |x| x < 0 || x >= row['input_base'] }
+    def invalid_digits?
+      row['digits'].any? { |x| x < 0 || x >= row['input_base'] }
     end
 
     def invalid_bases?
@@ -90,11 +90,11 @@ class AllYourBaseCase::PreProcessor
     end
 
     def input_of_zero?
-      row['input_digits'].all? { |x| x == 0 }
+      row['digits'].all? { |x| x == 0 }
     end
 
     def handle_special_cases
-      [4,2] if row['input_digits'] == [0, 6, 0]
+      [4,2] if row['digits'] == [0, 6, 0]
     end
   end
 end
@@ -102,7 +102,7 @@ end
 AllYourBaseCases = proc do |data|
   JSON.parse(data)['cases'].map.with_index do |row, i|
     AllYourBaseCase.new(
-      AllYourBaseCase::PreProcessor.call(row: row).merge(index: i),
+      AllYourBaseCase::PreProcessor[row].merge(index: i),
     )
   end
 end
